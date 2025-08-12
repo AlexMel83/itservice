7f9b99490c812f71fdb9edb6e8810b4d5a1b5738
Alexander Meleshko <meleshko.a.n@ukr.net>
2025-08-12 18:38:53 +0300

    +

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,11 +29,6 @@ export default defineNuxtConfig({
       head: {
         link: [
           {
-            rel: 'icon',
-            type: 'image/png',
-            href: '/icons/favicon.png',
-          },
-          {
             rel: 'stylesheet', 
             href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css', 
           },
